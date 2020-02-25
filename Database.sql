fb541fb0bfcd46b2122c9d12e243963c51f7365a
rrcookie <61480493+rrcookie@users.noreply.github.com>
2020-02-25 17:21:45 -0600

    Create Database.sql
    DDL and beginning DML of db

---
+++ Database.sql
@@ -0,0 +1,73 @@
+/* DDL statements below for entity sets: Organism, Taxonomy, Genome, Chromosomal, Mitochondrial
+*/
+
+CREATE TABLE Organism (
+	OrgID			CHAR(4)		NOT NULL,
+	Taxonomy		CHAR(4)		NOT NULL,
+	CommonName		CHAR(20)	NOT NULL,
+	ScientificName	CHAR(20)	NOT NULL,
+	FOREIGN KEY (Taxonomy) REFERENCES Taxonomy (TaxonID),
+	PRIMARY KEY	(SpeciesID)
+);
+CREATE TABLE Taxonomy (
+	TaxonID			CHAR(4)		NOT NULL,
+	TaxonGenus		CHAR(20)	NOT NULL,
+	TaxonFamily		CHAR(20)	NOT NULL,
+	TaxonOrder		CHAR(20)	NOT NULL,
+	TaxonClass		CHAR(20)	NOT NULL,
+	PRIMARY KEY (TaxonID),
+	UNIQUE KEY (Genus)
+);
+CREATE TABLE Genome (
+	AssemblyID			CHAR(4)		NOT NULL,
+	GenBankAccession	CHAR(20)	NOT NULL,
+	SpeciesID			CHAR(4)		NOT NULL,
+	ChromCount			INTEGER,
+	GenSize				INTEGER,
+	GC Content			INTEGER,
+	Protein				INTEGER,
+	RNA					INTEGER,
+	Gene				INTEGER,
+	FOREIGN KEY	(SpeciesID) REFERENCES	Organism (OrgID),
+	PRIMARY KEY (AssemblyID),
+	UNIQUE KEY	(GenBankAccession)
+);
+CREATE TABLE Chromosomal (
+	ChromoID	CHAR(4)		NOT NULL,
+	GenomeID	CHAR(4)		NOT NULL,
+	Name		CHAR(20)	NOT NULL,
+	CrSize		INTEGER,
+	GC Content 	INTEGER,
+	Protein 	INTEGER,
+	RNA			INTEGER,
+	Gene		INTEGER,
+	FOREIGN KEY	(GenomeID)	REFERENCES	Genome (AssemblyID),
+	PRIMARY KEY	(ChromoID)
+);
+CREATE TABLE Mitochondrial (
+	MtID		CHAR(4)		NOT NULL,
+	GenomeID	CHAR(4)		NOT NULL,
+	MtSize		INTEGER,
+	GC Content 	INTEGER,
+	Protein 	INTEGER,
+	RNA			INTEGER,
+	Gene		INTEGER,
+	FOREIGN KEY	(GenomeID)	REFERENCES	Genome (AssemblyID),
+	PRIMARY KEY	(MtID)
+);
+
+/*DML 
+*/
+INSERT INTO Taxonomy (TaxonID, TaxonGenus) VALUES
+		('T00', 'Felis'),
+		('T01', 'Lynx'),
+		('T02', 'Panthera'),
+		('T03', 'Acinonyx'),
+		('T04', 'Puma'),
+		('T05', 'Prionailurus');
+UPDATE Taxonomy
+SET		TaxonClass = 'Mammalia',
+		TaxonOrder = 'Carnivora',
+		TaxonFamily = 'Felidae';
+
+		
